6662247ee1456efe4b77b45cc32a7b551de9834a
Artem Rudov <dovakin2281337@gmail.com>
2024-03-21 00:40:54 +0300

    added expected files

--- _tests_/genDiff.test.js
+++ _tests_/genDiff.test.js
@@ -1,13 +1,17 @@
 import fs from 'fs';
 import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const path = '__tests__/__fixtures__/';
 
 describe('It works', () => {
-  it('json', () => {
+  test('json', () => {
     const before = `${path}file1.json`;
     const after = `${path}file2.json`;
     const content = fs.readFileSync(`${__dirname}/__fixtures__/expected.txt`, 'utf-8').trim();
     expect(genDiff(before, after)).toEqual(content);
-  })
+  });
 });

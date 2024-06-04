72f76a5665b6cef1da19fe8eddf4f7e942445a40
Artem Rudov <dovakin2281337@gmail.com>
2024-06-04 17:18:46 +0300

    cleaning tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,25 +5,25 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const path = '__tests__/__fixtures__/';
+const path = '__tests__/';
 
 describe('It works', () => {
   test('tree', () => {
     const before = `${path}filepath1.json`;
     const after = `${path}filepath2.json`;
-    const content = fs.readFileSync(`${__dirname}/__fixtures__/expectedPath.txt`, 'utf-8').trim();
+    const content = fs.readFileSync(`${__dirname}/expectedPath.txt`, 'utf-8').trim();
     expect(genDiff(before, after)).toEqual(content);
   });
   test('plain', () => {
     const before = `${path}filepath1.json`;
     const after = `${path}filepath2.json`;
-    const content = fs.readFileSync(`${__dirname}/__fixtures__/expectedPlain.txt`, 'utf-8').trim();
+    const content = fs.readFileSync(`${__dirname}/expectedPlain.txt`, 'utf-8').trim();
     expect(genDiff(before, after, 'plain')).toEqual(content);
   });
   test('jsonOutput', () => {
     const before = `${path}filepath1.json`;
     const after = `${path}filepath2.json`;
-    const content = fs.readFileSync(`${__dirname}/__fixtures__/expectedJson.txt`, 'utf-8').trim();
+    const content = fs.readFileSync(`${__dirname}/expectedJson.txt`, 'utf-8').trim();
     expect(genDiff(before, after, 'json')).toEqual(content);
   });
 });

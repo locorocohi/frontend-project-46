33351a67223b6a8b7727e209215868a9e38739e4
Artem Rudov <dovakin2281337@gmail.com>
2024-06-05 22:12:56 +0300

    replace fixtures

--- src/formaters/plainRender.js
+++ src/formaters/plainRender.js
@@ -14,20 +14,20 @@ const stringifyToPlain = (value) => {
 const plain = (diff) => {
   const iter = (curNode, prevPath) => {
     const lines = curNode.map((node) => {
-      const [key, type, value] = [getKey(node), getType(node), getValue(node)];
+      const [key, type] = [getKey(node), getType(node)];
       const curPath = [...prevPath, key];
       const baseLine = `Property '${curPath.join('.')}' was ${type}`;
       switch (type) {
         case 'added':
-          return `${baseLine} with value: ${stringifyToPlain(value)}`;
+          return `${baseLine} with value: ${stringifyToPlain(getValue(node))}`;
         case 'removed':
           return `${baseLine}`;
         case 'unchanged':
           return null;
         case 'updated':
-          return `${baseLine}. From ${stringifyToPlain(value[0])} to ${stringifyToPlain(value[1])}`;
+          return `${baseLine}. From ${stringifyToPlain(getValue(node)[0])} to ${stringifyToPlain(getValue(node)[1])}`;
         case 'nested':
-          return iter(value, curPath);
+          return iter(getValue(node), curPath);
         default:
           throw new Error(`Unsupported node type (${type})!`);
       }

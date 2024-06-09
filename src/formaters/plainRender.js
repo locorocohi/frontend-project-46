1130c0ea93151aa3c3f18c8968755f98215c9994
Artem Rudov <dovakin2281337@gmail.com>
2024-06-09 20:39:19 +0300

    refactor formaters

--- src/formaters/plainRender.js
+++ src/formaters/plainRender.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import { getKey, getType, getValue } from '../utils.js';
 
 const stringifyToPlain = (value) => {
   if (_.isPlainObject(value)) {
@@ -14,20 +13,20 @@ const stringifyToPlain = (value) => {
 const plain = (diff) => {
   const iter = (curNode, prevPath) => {
     const lines = curNode.map((node) => {
-      const [key, type] = [getKey(node), getType(node)];
+      const [key, type] = [node.key, node.type];
       const curPath = [...prevPath, key];
       const baseLine = `Property '${curPath.join('.')}' was ${type}`;
       switch (type) {
         case 'added':
-          return `${baseLine} with value: ${stringifyToPlain(getValue(node))}`;
+          return `${baseLine} with value: ${stringifyToPlain(node.value)}`;
         case 'removed':
           return `${baseLine}`;
         case 'unchanged':
           return null;
         case 'updated':
-          return `${baseLine}. From ${stringifyToPlain(getValue(node)[0])} to ${stringifyToPlain(getValue(node)[1])}`;
+          return `${baseLine}. From ${stringifyToPlain(node.data1)} to ${stringifyToPlain(node.data2)}`;
         case 'nested':
-          return iter(getValue(node), curPath);
+          return iter(node.children, curPath);
         default:
           throw new Error(`Unsupported node type (${type})!`);
       }
--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import { getKey, getValue, getType } from '../utils.js';
 
 const stringify = (value, currentDepth) => {
   const iter = (currentValue, depth) => {
@@ -24,22 +23,22 @@ const stylish = (diff) => {
     const indent = createIdent(depth);
     const bracketIndent = '    '.repeat(depth - 1);
     const lines = currentNode.map((node) => {
-      const [key, type, value] = [getKey(node), getType(node), getValue(node)];
+      const [key, type] = [node.key, node.type];
 
       switch (type) {
         case 'added':
-          return `${indent}+ ${key}: ${stringify(value, depth + 1)}`;
+          return `${indent}+ ${key}: ${stringify(node.value, depth + 1)}`;
         case 'removed':
-          return `${indent}- ${key}: ${stringify(value, depth + 1)}`;
+          return `${indent}- ${key}: ${stringify(node.value, depth + 1)}`;
         case 'unchanged':
-          return `${indent}  ${key}: ${stringify(value, depth + 1)}`;
+          return `${indent}  ${key}: ${stringify(node.value, depth + 1)}`;
         case 'updated':
           return [
-            `${indent}- ${key}: ${stringify(value[0], depth + 1)}`,
-            `${indent}+ ${key}: ${stringify(value[1], depth + 1)}`,
+            `${indent}- ${key}: ${stringify(node.data1, depth + 1)}`,
+            `${indent}+ ${key}: ${stringify(node.data2, depth + 1)}`,
           ].join('\n');
         case 'nested':
-          return `${indent}  ${key}: ${iter(value, depth + 1)}`;
+          return `${indent}  ${key}: ${iter(node.children, depth + 1)}`;
         default:
           throw new Error(`Unsupported node type (${type})!`);
       }
